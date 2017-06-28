182cc864a5bbf0eedd9637fdc1eacec96419c74a
Furkan Türkal <furkan.turkal@hotmail.com>
2017-06-28 17:02:36 +0300

    Small improvements

--- Maxiploit/Forms/VSRO/frmVSRO.Designer.cs
+++ Maxiploit/Forms/VSRO/frmVSRO.Designer.cs
@@ -1124,7 +1124,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(62, 34);
+            this.checkBox1.Location = new System.Drawing.Point(52, 42);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 1;
